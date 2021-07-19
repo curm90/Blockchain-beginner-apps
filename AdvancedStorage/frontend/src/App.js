15febeb6827d4e853ff8bed5e9d8b08473c51fbd
Liam Sutton <devliam90@gmail.com>
2021-07-19 13:07:31 +0100

    get addItem working

--- AdvancedStorage/frontend/src/App.js
+++ AdvancedStorage/frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import web3 from './web3';
+import initWeb3 from './web3';
+import { Container, Form, Input, Message, Button } from 'semantic-ui-react';
 import { ADDRESS, ABI } from './advancedStorage';
 
 class App extends React.Component {
   state = {
     contract: undefined,
+    inputVal: '',
+    errorMessage: '',
+    accounts: [],
+    numbers: undefined,
+    web3: undefined,
   };
 
   componentDidMount() {
@@ -12,15 +18,71 @@ class App extends React.Component {
   }
 
   loadBlockchainData = async () => {
+    let web3 = await initWeb3();
     const contract = new web3.eth.Contract(ABI, ADDRESS);
-    this.setState(() => ({ contract }));
+    const accounts = await web3.eth.getAccounts();
+    const numbers = await contract.methods.getAll().call();
+    this.setState(() => ({ contract, accounts, numbers, web3 }));
+    console.log(this.state);
+  };
+
+  onChange = (e) => {
+    this.setState(() => ({ inputVal: e.target.value, errorMessage: '' }));
+  };
+
+  addItem = async () => {
+    try {
+      await this.state.contract.methods
+        .addItem(this.state.web3.utils.toBN(this.state.inputVal))
+        .send({ from: this.state.accounts[0] });
+    } catch (error) {
+      this.setState(() => ({ errorMessage: error.message }));
+    }
+
+    const numbers = await this.state.contract.methods.getAll().call();
+    this.setState(() => ({ ...this.state, numbers, inputVal: '' }));
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    this.addItem();
   };
 
   render() {
     return (
-      <div className="App">
+      <Container className="App" style={{ marginTop: '50px' }}>
         <h1>Advanced storage</h1>
-      </div>
+        <h4>Welcome {this.state.accounts[0]}</h4>
+
+        <div>
+          <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+            <Form.Field>
+              <label>Add item (number)</label>
+              <Input
+                value={this.state.inputVal}
+                placeholder="Add new item..."
+                onChange={this.onChange}
+                type="number"
+              ></Input>
+            </Form.Field>
+            <Button primary>Add</Button>
+            <Message
+              error
+              header="Oops!"
+              content={this.state.errorMessage}
+            ></Message>
+          </Form>
+        </div>
+        <div style={{ marginTop: '50px', borderTop: '1px solid #eee' }}></div>
+        <ul>
+          {this.state.numbers ? (
+            this.state.numbers.map((number, i) => <li key={i}>{number}</li>)
+          ) : (
+            <h4>Add an item to begin</h4>
+          )}
+        </ul>
+      </Container>
     );
   }
 }
--- AdvancedStorage/frontend/src/web3.js
+++ AdvancedStorage/frontend/src/web3.js
@@ -1,30 +1,27 @@
 import Web3 from 'web3';
 
-let web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+function initWeb3() {
+  return new Promise((resolve, reject) => {
+    // Case 1: new metamask is present
+    if (typeof window.ethereum !== 'undefined') {
+      window.ethereum
+        .enable()
+        .then(() => {
+          resolve(new Web3(window.ethereum));
+        })
+        .catch((err) => {
+          reject(err);
+        });
+      return;
+    }
+    // Case 2: old metamask is present
+    if (typeof window.web3 !== 'undefined') {
+      return resolve(new Web3(window.web3.currentProvider()));
+    }
 
-export default web3;
+    // Case 3: no metamask present just connect to Ganache
+    resolve(new Web3('http://localhost:9545'));
+  });
+}
 
-// function initWeb3() {
-// return new Promise((resolve, reject) => {
-//   // Case 1: old metamask is present
-//   if (typeof window.web3 !== 'undefined') {
-//     return resolve(new Web3(window.web3.currentProvider()));
-//   }
-//   // Case 2: new metamask is present
-//   if (typeof window.ethereum !== 'undefined') {
-//     window.ethereum
-//       .enable()
-//       .then(() => {
-//         resolve(new Web3(window.ethereum));
-//       })
-//       .catch((err) => {
-//         reject(err);
-//       });
-//     return;
-//   }
-//   // Case 3: no metamask present just connect to Ganache
-//   resolve(new Web3('http://localhost:9545'));
-// });
-// }
-
-// export default initWeb3;
+export default initWeb3;
