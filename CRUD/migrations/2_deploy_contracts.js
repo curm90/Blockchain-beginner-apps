f96d70e2079b71a470a3550b3a88c3a11f8b2296
Liam Sutton <devliam90@gmail.com>
2021-07-19 15:27:10 +0100

    test smart contract

--- CRUD/migrations/2_deploy_contracts.js
+++ CRUD/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Crud = artifacts.require('./Crud.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Crud);
 };
